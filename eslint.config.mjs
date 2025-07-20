a59b2fd14b2fd003c013043ee85768b285f18ddb
p1n9d3v <p1n9m4i1@gmail.com>
2025-07-20 15:09:51 +0900

    chore: eslint 및 불필요한 파일 정리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,63 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import perfectionist from 'eslint-plugin-perfectionist';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        plugins: {
+            perfectionist,
+        },
+        rules: {
+            'perfectionist/sort-imports': [
+                'error',
+                {
+                    type: 'alphabetical',
+                    order: 'asc',
+                    fallbackSort: { type: 'unsorted' },
+                    ignoreCase: true,
+                    specialCharacters: 'keep',
+                    internalPattern: ['^~/.+', '^@/.+'],
+                    partitionByComment: false,
+                    partitionByNewLine: false,
+                    newlinesBetween: 1,
+                    maxLineLength: undefined,
+                    groups: [
+                        'type-import',
+                        ['value-builtin', 'value-external'],
+                        'type-internal',
+                        'value-internal',
+                        ['type-parent', 'type-sibling', 'type-index'],
+                        ['value-parent', 'value-sibling', 'value-index'],
+                        'ts-equals-import',
+                        'unknown',
+                    ],
+                    customGroups: [],
+                    environment: 'node',
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {
+            '@typescript-eslint/consistent-type-imports': [
+                'error',
+                {
+                    prefer: 'type-imports',
+                    disallowTypeAnnotations: false,
+                },
+            ],
+        },
+    },
 ];
 
 export default eslintConfig;
---
+++ src/components/types.ts
@@ -0,0 +1,3 @@
+export type TypeTest = {
+    name: string;
+};
